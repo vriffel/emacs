ef4419b8a554dc6c133281c7aea60140cb4818c6
Vinicius Riffel <viniciusricardoriffel@gmail.com>
2021-05-28 21:06:06 -0300

    improves init

--- init.el
+++ init.el
@@ -143,21 +143,3 @@
    (setq comint-move-point-for-output t)))
 
 (load-theme 'darktooth t)
-
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("c9ddf33b383e74dac7690255dd2c3dfa1961a8e8a1d20e401c6572febef61045" default))
- '(package-selected-packages
-   '(darktooth-theme company smartparens ess-view ess poly-R poly-markdown polymode ample-theme bind-key)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
